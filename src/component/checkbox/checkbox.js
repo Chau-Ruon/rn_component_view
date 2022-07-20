13bec1ed8088fa6a868dfe32b3920221698eb8d6
chauruon <chauruon.lci@gmail.com>
2022-07-20 15:26:26 +0700

    asfsdfjsahf

---
+++ src/component/checkbox/checkbox.js
@@ -0,0 +1,19 @@
+import { 
+    View,
+    Text,
+} from 'react-native'
+import React,{
+    useEffect,
+    useState,
+    useRef,
+} from 'react'
+
+const checkbox = () => {
+  return (
+    <View>
+      <Text>checkbox</Text>
+    </View>
+  )
+}
+
+export default checkbox
---
+++ src/component/checkbox/checkbox.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { 
+    View,
+    TouchableOpacity,
+    StyleProp,
+    ViewStyle,
+ } from 'react-native'
+
+interface props {
+    
+}
+
+const checkbox = (
+    {
+
+    }:props
+) => {
+  return (
+    <View>FSKDJFHKJ</View>
+  )
+}
